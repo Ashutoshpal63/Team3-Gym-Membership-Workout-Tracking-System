558d5cf583fcedf5611aff2ee3afe99620251caf
Ashutoshpal63 <ashutoshpal646@gmail.com>
2025-12-04 17:52:04 +0000

    add  app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,4 +35,10 @@ function AppContent() {
   return <LandingPage onRoleSelect={() => setShowLogin(true)} showLogin={showLogin} onCloseLogin={() => setShowLogin(false)} />;
 }
 
-
+export default function App() {
+  return (
+    <AuthProvider>
+      <AppContent />
+    </AuthProvider>
+  );
+}
